6392642c023eb82abfc9bcbc46ee1f8d02dea044
sczhaoqi <1151324240@qq.com>
2018-11-12 16:44:16 +0800

    Update DiscoveryUpdator.java
    fixed:
    remove unuseless reflect

--- src/main/java/com/sc/zhaoqi/web/modules/discovery/DiscoveryUpdator.java
+++ src/main/java/com/sc/zhaoqi/web/modules/discovery/DiscoveryUpdator.java
@@ -2,18 +2,13 @@ package com.sc.zhaoqi.web.modules.discovery;
 
 import com.google.inject.Inject;
 import io.airlift.concurrent.Threads;
-import io.airlift.discovery.client.ServiceInventory;
-import io.airlift.discovery.client.ServiceInventoryConfig;
+import io.airlift.discovery.client.DiscoveryClientConfig;
 import io.airlift.http.server.HttpServerInfo;
 import io.airlift.log.Logger;
-import io.airlift.node.NodeInfo;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
-import java.lang.reflect.Field;
-import java.net.URI;
-import java.net.URISyntaxException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -23,25 +18,21 @@ public class DiscoveryUpdator
 {
     private static final Logger log = Logger.get(DiscoveryUpdator.class);
 
-    private ServiceInventory inventory;
+    private final DiscoveryClientConfig config;
     private HttpServerInfo info;
     private ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor(Threads.daemonThreadsNamed("service-inventory-%s"));
     private ScheduledFuture<?> scheduledFuture;
 
     @Inject
-    public DiscoveryUpdator(ServiceInventory inventory, HttpServerInfo info)
+    public DiscoveryUpdator(DiscoveryClientConfig config, HttpServerInfo info)
     {
-        this.inventory = inventory;
         this.info = info;
+        this.config = config;
     }
 
     private void updateServiceInventory()
-            throws NoSuchFieldException, IllegalAccessException, URISyntaxException
     {
-        Field inventoryField = inventory.getClass().getDeclaredField("serviceInventoryUri");
-        inventoryField.setAccessible(true);
-        inventoryField.set(inventory, info.getHttpUri());
-        inventory.updateServiceInventory();
+        config.setDiscoveryServiceURI(info.getHttpUri());
     }
 
     @PostConstruct
@@ -54,7 +45,7 @@ public class DiscoveryUpdator
                 updateServiceInventory();
             }
             catch (Exception e) {
-                log.error("failed update service Inventory"+ e);
+                log.error("failed update service Inventory" + e);
             }
         }, 1000, 3000, TimeUnit.MILLISECONDS);
     }
